32f33a3b3d432fdcea2903c8ceb3979826b67234
cdtest <corydavis1989@gmail.com>
2022-02-15 16:29:11 -0600

    added voters table and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
+DROP TABLE IF EXISTS voters;
 
 CREATE DATABASE just_tech_news_db;
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
